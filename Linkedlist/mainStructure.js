13e62a2a45365c8ed8954e8476a93d7a0900b305
unknown <muh.it.eng.19841984@gmail.com>
2022-09-22 15:36:07 +0300

    linked list - add to first

---
+++ Linkedlist/mainStructure.js
@@ -0,0 +1,24 @@
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+class LinkeList {
+  constructor() {
+    this.head = null;
+    this.size = 0;
+  }
+  addFirst(data) {
+    // here data and next
+    this.head = new Node(data, this.head);
+  }
+}
+
+node = new LinkeList();
+node.addFirst(10);
+node.addFirst(20);
+node.addFirst(30);
+
+console.log(node);
