88f4f51095b53d164f06b37bcb21b8369e0041e1
muh.it.eng.19841984@gmail.com <muh.it.eng.19841984@gmail.com>
2024-01-24 09:47:25 -0800

    traverse during linked list

--- Linkedlist/mainStructure.js
+++ Linkedlist/mainStructure.js
@@ -15,9 +15,19 @@ node2 = new LinkeList(23);
 node3 = new LinkeList(10);
 node4 = new LinkeList(60);
 
+//link nodes
 node1.next=node2
 node2.next=node3
 node3.next=node4
 
+// create ptr cursor for traverse during nodes
+let i=1
+ptr=node1
+while(ptr !=null ){
+console.log(`node ${i} is : ${ptr.value} `)
+ptr=ptr.next
+i++
+}
 
-console.log(JSON.stringify(node1,null,2));
+
+//console.log(JSON.stringify(node1,null,2));
